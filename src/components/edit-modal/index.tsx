9bd450acd0ffb458aa9cbd8a1ee183593ebd4c4d
たむらけいと <tamurakeito@chibikeinoiMac.local>
2023-12-05 19:17:01 +0900

    文字数制限

--- src/components/edit-modal/index.tsx
+++ src/components/edit-modal/index.tsx
@@ -25,6 +25,17 @@ export const EditModal = ({
   const { tab } = useTabContext();
   const { setIsError } = useErrorContext();
   const { setToast } = useToastContext();
+
+  const chaLimit = 20;
+  const onChange = (value: string) => {
+    value.length > chaLimit
+      ? setToast({
+          content: "20文字以内で登録してください",
+          isSuccess: false,
+        })
+      : setValue(value);
+  };
+
   const handleExec = async () => {
     setIsLoading(true);
     if (tab !== undefined) {
@@ -69,7 +80,7 @@ export const EditModal = ({
       <div className={"EditModal"}>
         <InputBox
           value={value}
-          onChange={setValue}
+          onChange={onChange}
           icon={InputIcon.edit3}
           button={InputButton.check}
           handleOnBlur={() => setIsActive(false)}
