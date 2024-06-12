e530cc36298fa00e8da8a1cc87ddcddbb9996faf
tamurakeito <tamurakeito@chibikei-Mac-mini.local>
2024-06-12 20:59:43 +0900

    yarn commit: 環境変数調整

--- src/data/axios.ts
+++ src/data/axios.ts
@@ -4,15 +4,3 @@ export const client = axios.create({
   baseURL: process.env.REACT_APP_AXIOS_BASE_URL,
   timeout: 15000,
 });
-
-// 必要なAPI
-// 各メモの詳細を取得する
-// メモの一覧を取得する
-// メモ名を変更する
-// メモのタグステータスを変更する
-// メモを追加する
-// メモを削除する
-// タスクステータスを変更する（複数）
-// タスクを追加する
-// タスクを削除する（複数）
-//
